0cf1fd835385c55b055a64f27b05220955dfa73c
Leo Gall <gall.dev@proton.me>
2025-02-02 16:23:09 +0100

    clean up code and migrate analytics to posthog

---
+++ assets/js/dark-mode.js
@@ -0,0 +1,19 @@
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+function toggleDarkMode() {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  }
+}
---
+++ assets/js/validation.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+  var emailField = document.getElementById("field_3");
+  emailField.setAttribute("type", "email");
+};
